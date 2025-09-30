5a531250f0da4b430d0fcb144d366d71dccce68b
umaaneja <102696553+umaaneja@users.noreply.github.com>
2025-09-30 23:50:18 +0100

    Create identy.py

---
+++ identy.py
@@ -0,0 +1,10 @@
+  Identity identity = context.getObjectByName(Identity.class, identityName);
+    if (identity == null) {
+        System.out.println("❌ Identity not found: " + identityName);
+    } else {
+        for (Link link : identity.getLinks()) {
+            if (link.getApplicationName().equalsIgnoreCase(appName)) {
+                System.out.println("✅ Azure AD nativeIdentity = " + link.getNativeIdentity());
+            }
+        }
+    }
